2f51474643286f7730a9dd78640fd4e89dfa55d9
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2017-12-13 20:36:38 -0800

    pixel field moved to separate class

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,14 @@
 #include <vector>
 #include <iostream>
 #include "declarations.h"
+#include "pixel_field.h"
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+
 using namespace std;
 
-#define TILE_SIZE 10
-#define IMAGE_WIDTH 28
+
 
 void showVectorVals(string label, vector<double> &v)
 {
@@ -62,57 +63,10 @@ void trainXor() {
 	cout << endl << "Done" << endl;
 }
 
-struct Field
-{
-	sf::RectangleShape pixels[IMAGE_WIDTH][IMAGE_WIDTH];
 
-	Field()
-	{
-		for (int i = 0; i < IMAGE_WIDTH; i++)
-		{
-			for (int j = 0; j < IMAGE_WIDTH; j++)
-			{
-				pixels[i][j].setPosition(i, j);
-				pixels[i][j].setFillColor(sf::Color::White);
-				pixels[i][j].setSize(sf::Vector2f(1, 1));
-				pixels[i][j].setOutlineThickness(0.1);
-				pixels[i][j].setOutlineColor(sf::Color(128, 128, 128));
-			}
-		}
-	}
-
-	void draw(sf::RenderTarget& renderTarget)
-	{
-		for (int i = 0; i < IMAGE_WIDTH; i++)
-		{
-			for (int j = 0; j < IMAGE_WIDTH; j++)
-			{
-				renderTarget.draw(pixels[i][j]);
-			}
-		}
-	}
-
-	void reset()
-	{
-		for (int i = 0; i < IMAGE_WIDTH; i++)
-		{
-			for (int j = 0; j < IMAGE_WIDTH; j++)
-			{
-				pixels[i][j].setFillColor(sf::Color::White);
-			}
-		}
-	}
-
-	void paint(int i, int j)
-	{
-		pixels[i][j].setFillColor(sf::Color::Black);
-	}
-};
 
 int main()
 {
-	//trainXor();
-
 	sf::RenderWindow window(sf::VideoMode(IMAGE_WIDTH * TILE_SIZE, IMAGE_WIDTH * TILE_SIZE), "input");
 	window.setFramerateLimit(60);
 
@@ -175,9 +129,7 @@ int main()
 			}
 		}
 		window.setActive();
-
 		field.draw(window);
-
 		window.display();
 	}
 
---
+++ source/pixel_field.h
@@ -0,0 +1,60 @@
+//
+// Created by Owntage on 12/13/2017.
+//
+
+#ifndef NETWORK_SHOOTER_PROJECT_PIXEL_FIELD_H
+#define NETWORK_SHOOTER_PROJECT_PIXEL_FIELD_H
+
+#include <SFML/Graphics.hpp>
+
+#define TILE_SIZE 10
+#define IMAGE_WIDTH 28
+
+struct Field
+{
+	sf::RectangleShape pixels[IMAGE_WIDTH][IMAGE_WIDTH];
+
+	Field()
+	{
+		for (int i = 0; i < IMAGE_WIDTH; i++)
+		{
+			for (int j = 0; j < IMAGE_WIDTH; j++)
+			{
+				pixels[i][j].setPosition(i, j);
+				pixels[i][j].setFillColor(sf::Color::White);
+				pixels[i][j].setSize(sf::Vector2f(1, 1));
+				pixels[i][j].setOutlineThickness(0.1);
+				pixels[i][j].setOutlineColor(sf::Color(128, 128, 128));
+			}
+		}
+	}
+
+	void draw(sf::RenderTarget& renderTarget)
+	{
+		for (int i = 0; i < IMAGE_WIDTH; i++)
+		{
+			for (int j = 0; j < IMAGE_WIDTH; j++)
+			{
+				renderTarget.draw(pixels[i][j]);
+			}
+		}
+	}
+
+	void reset()
+	{
+		for (int i = 0; i < IMAGE_WIDTH; i++)
+		{
+			for (int j = 0; j < IMAGE_WIDTH; j++)
+			{
+				pixels[i][j].setFillColor(sf::Color::White);
+			}
+		}
+	}
+
+	void paint(int i, int j)
+	{
+		pixels[i][j].setFillColor(sf::Color::Black);
+	}
+};
+
+#endif //NETWORK_SHOOTER_PROJECT_PIXEL_FIELD_H
