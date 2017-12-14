20c09015e0c7e236a6e94b6ea6fd3957d0910c0b
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2017-12-13 19:18:41 -0800

    separation finished

--- source/neural-net.cpp
+++ source/neural-net.cpp
@@ -1,14 +1,19 @@
 #include <vector>
 #include <iostream>
-#include <cstdlib>
-#include <cassert>
-#include <cmath>
-#include <fstream>
-#include <sstream>
 #include "declarations.h"
 
 using namespace std;
 
+
+void showVectorVals(string label, vector<double> &v)
+{
+	cout << label << " ";
+	for(unsigned i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
 
 int main()
 {
